3682b2c8ce22906d67e12f4c0957794fbb0a988c
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2023-11-16 17:46:55 +0530

    create all apis for todo

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
+const TodoSchema = require('./Models/todo')
+dotenv.config();
+
+connectDB()
+
+app.use(express.json({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT
+
+app.get('/getTodos', async (req, res) => {
+  var todo = await TodoSchema.find({})
+  res.status(200).send({ todos: todo })
+})
+
+app.post('/addTodo', async (req, res) => {
+
+  const todo = new TodoSchema({
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status
+  });
+  let response = await todo.save();
+  res.status(201).json(response);
+});
+
+
+app.put('/updateTodo/:id', async (req, res) => {
+  try {
+    var todo = await TodoSchema.findById(req.params.id)
+    todo.completed = !todo.completed
+    await todo.save()
+    res.send({ message: "Horray!! You completed task successfully" })
+
+  } catch (error) {
+    res.send({ message: error.message })
+  }
+})
+
+app.delete('/deleteTodo/:id', async (req, res) => {
+  try {
+    var todo = await TodoSchema.findByIdAndDelete(req.params.id)
+    res.send({ message: "Todo Deleted" })
+
+  } catch (error) {
+    res.send({ message: error.message })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
